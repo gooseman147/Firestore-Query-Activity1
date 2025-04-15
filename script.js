231d6a2b7b83afae96ce4bf9a7a6c06e768bc049
Gersmeyer <myagersmeyer28@gmail.com>
2025-04-14 22:28:27 -0500

    changes

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+team name Real Madrid 
+city: Madrid 
+Country: Spain 
+top scorers: Ronaldo, Benzema, Hazard
+Worldwide fans (in millions): 798 
+
+let rm = {
+    name: "real madrid", 
+    city: "madrid", 
+    country: "spain",
+
+}
